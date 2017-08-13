7073f59ad5db6ba5b8090e8acce97796da4acbd4
jinhengs <fredj73@gmail.com>
2017-08-12 23:31:00 -0400

    added heuristics for solution

--- course_planner.py
+++ course_planner.py
@@ -2,12 +2,16 @@ from course_database import *
 from course_planner_csp import *
 from propagators import *
 from orderings import *
+from filter import *
 
 COURSES = []
 SUMMER = False
 YEARS = 0
 UNAVBL = []
+PREF_TIME = []
+PROF_PREF = 1
 curr_dat = dat
+SOL = []
 
 if __name__ == "__main__":
 	yrs = input("How many long do you plan on going to school(years):")
@@ -50,7 +54,19 @@ if __name__ == "__main__":
 			UNAVBL.append(inp)
 		elif(inp != 'done'):
 			print("invalid input")
-	
+	print("What are your preferred timeslots? Enter 'done' when finished:")
+	print("(FORMAT : M,T,W,J,F = Mon, Tues, Wed, Thur, Fri")
+	print("		M,D,E,N = Morning, Day, Evening, Night")
+	print(" Example: I prefer to take classes on Tuesday Evenings -> 'TE')")
+	inp2 = ""
+	while(inp2 != 'done'):
+		inp2 = input("")
+		if (inp2[0] in ["M", "T", "W", "J", "F"] and inp2[1] in ["M","D","E","N"]):
+			PREF_TIME.append(inp2)
+		elif(inp2 != 'done'):
+			print("invalid input")
+	PROF_PREF = input("Rate the importance of the quality of professors from 1-5\n1 being not important and 5 being very important: ")
+	PROF_PREF = int(PROF_PREF)
 	soln = dict()
 	while soln != None:
 		csp, var_array = course_planner_csp(COURSES, SUMMER, YEARS, UNAVBL, curr_dat)
@@ -58,7 +74,11 @@ if __name__ == "__main__":
 		soln = solver.bt_search(prop_GAC)
 		if soln != None:
 			print(soln)
+			SOL.append(soln)
 			for course in soln.keys():
 				curr_dat[course][2].remove(soln[course])
-		
+	sol = get_best_solution(SOL, PREF_TIME, PROF_PREF)
+	print("best solution\n")
+	print(sol)
+
 		
