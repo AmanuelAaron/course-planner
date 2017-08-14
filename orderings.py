4d2dff62de687ddfe501230d5b5eb240434b5e92
jinhengs <fredj73@gmail.com>
2017-08-14 13:39:50 -0400

    legacy code for old heuristics(Doesn't work either, global variables can't be used this way, easy fix however)

--- orderings.py
+++ orderings.py
@@ -1,6 +1,8 @@
-#Look for #IMPLEMENT tags in this file. These tags indicate what has
+  #Look for #IMPLEMENT tags in this file. These tags indicate what has
 #to be implemented.
-
+from course_planner import *
+from course_database import *
+RATIO = 0.1
 import random
 
 '''
@@ -39,5 +41,38 @@ def ord_mrv(csp):
     		md = v.cur_domain_size()
     		mv = v
     return mv
+
+#First attempt at creating an efficient way of predicting how variables will be expanded
+#Similar to the final version of heuristics, but this tries to predict the future
+#Since the results are not known, the order of values for variables are taken into consideration
+#for predicting the future.
+#The final heuristic function does not take order into account since it knows all the results
+def ord_weighted(listx):
+	'''
+	:param list of var:
+	:return variable:
+	 greedy heuristic that returns the variable with the lowest heuristic value.
+	 idealy trading performance for optimal schedule based on preferences
+	'''
+	mv = None
+	current = None
+	for v in listx:
+		#add timeslot h_val
+		h_value = ((v.cur_domain_size()+1)*(v.cur_domain_size()))/2
+		#add professor rating h_val
+		h_value += h_val*RATIO*PROF_PREF*6
+		temp = v.cur_domain();
+		for i in range(len(temp)):
+			for timeslot in PREF_TIME:
+				if temp[i][0][1] == timeslot[0] and temp[i][0][2] == timeslot[1]:
+					h_value = h_value - len(temp) + i
+			h_value -= (1-(i/len(temp)))*prof_rat[temp[i][0][3]]*RATIO*PROF_PREF
+		if current == None:
+			mv = v
+			current = h_value
+		elif h_value < current:
+			mv = v
+			current = h_value
+	return mv
 
 
